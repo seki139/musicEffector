f8c5d28e7c8ea447e113bd813b3bcb88f3f8400a
damin <gritdz555@gmail.com>
2024-11-08 22:56:20 +0900

    Update connect.ts

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "Tk?rz139",
-  database: process.env.DB_NAME || "music",
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASS ,
+  database: process.env.DB_NAME ,
 });
